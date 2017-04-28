dd3049353e31ce1c373b335a5864d9362094c596
Kenny Lu <klu@realmatters.com>
2017-04-28 11:43:50 -0400

    lesson 8 - escape html chars with a pre-made function from SO but the important point is calling this helper-fn at the sub-fn

--- 08-decorators/index.js
+++ 08-decorators/index.js
@@ -4,9 +4,20 @@ var escape = require('escape-html');
 
 var app = module.exports = koa();
 
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+ }
+
 app.use(function* (next) {
   yield next;
   // add some logic here!
+
+  this.body = escapeHtml(this.body);
 })
 
 app.use(function* body() {

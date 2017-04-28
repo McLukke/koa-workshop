c6f5643c85568e257172bab0f0ebe6331f31a700
Kenny Lu <klu@realmatters.com>
2017-04-28 11:32:09 -0400

    lesson 7 - set json headers only when request is json

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -4,5 +4,13 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function* () {
-  
+  if (this.request.is('application/json') == 'application/json') {
+    this.response.type = 'application/json';
+
+    this.body = {
+      message: 'hi!',
+    };
+  } else {
+    this.body = 'ok';
+  }
 })
